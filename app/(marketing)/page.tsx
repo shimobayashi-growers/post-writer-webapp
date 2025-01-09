83ea153134819561aa7184798b8b148194a84600
Takaaki Shimobayashi <takaaki.shimobayashi@growers.co.jp>
2025-01-09 16:44:10 +0900

    refactor: configとtypesの追加

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,18 +1,19 @@
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/button";
+import { siteConfig } from "@/config/site";
 
 export default function IndexPage() {
   return (
     <>
         <section className="pt-6 md:pt-10 lg:py-32 pb-8 md:pb-12">
             <div className="container text-center flex flex-col items-center gap-4 max-w-[64lem]">
-                <Link href={"/"} className="bg-muted px-4 py-1.5 rounded-2xl font-medium text-sm">Xをフォローする</Link>
+                <Link href={siteConfig.links.x} className="bg-muted px-4 py-1.5 rounded-2xl font-medium text-sm" target="_blank" rel="noreferrer">Xをフォローする</Link>
                 <h1 className="font-extrabold text-3xl sm:text-5xl md:text-6xl lg:text-7xl">Post Writer</h1>
                 <p className="text-muted-foreground sm:text-xl leading-normal max-w-[42rem]">Post WriterはNext.js 14 App Routerで作られたサービスです。ユーザは自由に投稿をポストすることができます</p>
                 <div className="space-x-4">
                     <Link href={"/login"} className={cn(buttonVariants({size:"lg"}))}>はじめる</Link>
-                    <Link href={"/"} className={cn(buttonVariants({size:"lg", variant:"outline"}))} target="_blank" rel="noreferrer">Github</Link>
+                    <Link href={siteConfig.links.github} className={cn(buttonVariants({size:"lg", variant:"outline"}))} target="_blank" rel="noreferrer">Github</Link>
                 </div>
             </div>
         </section>
---
+++ config/site.ts
@@ -0,0 +1,12 @@
+import type { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+    name: "Post Writer",
+    description: "Post WriterはNext.js 14 App Routerで作られたサービスです。ユーザは自由に投稿をポストすることができます",
+    url: "https://localhost:3000",
+    ogImage: "",
+    links: {
+        x: "https://x.com/bayashimo",
+        github: "https://github.com/shimobayashi-growers",
+    },
+}
